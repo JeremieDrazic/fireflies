b4c0ebb99985fec1bf286a91ad65171b83a045e8
Jeremie Drazic <jerem.drazic@gmail.com>
2021-06-12 23:24:10 +0200

    eslint final

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,41 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: ['airbnb-typescript/base', 'prettier'],
-  plugins: ['prettier', '@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   parserOptions: {
+    sourceType: 'module',
     project: './tsconfig.json',
   },
   env: {
     browser: true,
-    node: true,
-    jasmine: true,
-    jest: true,
+  },
+  rules: {
+    // eslint typescript plugin rules override
+    '@typescript-eslint/no-throw-literal': 'warn',
+    '@typescript-eslint/semi': 'off',
+
+    // eslint import plugin rules
+    'import/no-unresolved': 'off',
+    'import/no-named-as-default': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'never',
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'never',
+      {
+        json: 'always',
+        png: 'always',
+        svg: 'always',
+        jpg: 'always',
+        jpeg: 'always',
+      },
+    ],
   },
 }
