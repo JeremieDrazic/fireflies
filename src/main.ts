269b06c5e682e049cc7ef589106db81ae774c2d6
Jeremie Drazic <jerem.drazic@gmail.com>
2021-06-20 19:33:27 +0200

    refacto elements

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import App from 'src/components/App'
-import { APP } from 'src/utils/constants'
-import { ElementType } from 'src/utils/types'
 import 'src/styles/reset.css'
 import 'src/styles/theme.css'
 
 const init = () => {
-  const app = new App(APP as ElementType)
+  const app = new App()
   app.start()
 }
 
